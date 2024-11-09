b4fc48fb7b0aca5eb772f2f7236c0949d7477730
Dvir Uiel <dviruiel@h-MacBook-Pro-sl-Dvir.local>
2024-11-09 17:47:20 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import AboutView from './Components/AboutView';
+import {Routes,Route} from 'react-router-dom';
+import SearchView from './Components/SearchView';
+import { useState,useEffect } from 'react';
+import MovieView from './Components/MovieView';
+import NotFound from './Components/NotFound';
 
 function App() {
+  const [searchResults , setSearchResults] = useState([]);
+  const [searchText , setSearchText] = useState('');
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    if(searchText){
+      fetch(`${apiUrl}/search/movie?query=${searchText}&api_key=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        setSearchResults(data.results)
+      })}
+  }, [apiUrl,apiKey,searchText])
+    
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Navbar searchText={searchText} setSearchText={setSearchText} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='About/' element={<AboutView />} />
+        <Route path='Search/' element={<SearchView keyword={searchText} searchResults={searchResults} />} />
+        <Route path='/movie/:id' element={<MovieView />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>      
     </div>
   );
 }
